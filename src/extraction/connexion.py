54d674db34dcee4fb0878ba0381a744f33a4ace0
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2024-02-27 16:06:54 +0100

    ok

--- src/extraction/connexion.py
+++ src/extraction/connexion.py
@@ -1,4 +1,5 @@
 import pyodbc
+import pandas as pd 
 
 from lock_parameters import server, database, username, password
 
@@ -7,28 +8,32 @@ connection = pyodbc.connect(driver='{SQL Server}', host=server, port=1433, datab
                       trusted_connection='No', user=username, password=password)
 
 cursor = connection.cursor()
-
+df = pd.read_excel("./ipp_list.xlsx")
+ipp_list = df['IPP'].unique().tolist()  # Get unique IPPs
+ipp_str = ','.join([f"'{ipp}'" for ipp in ipp_list])
+print(ipp_str)
 # Sample query
 query = """select p.pat_ipp, d.doc_nom, d.doc_creation_date, d.doc_realisation_date, d.doc_stockage_id, fil_data
             from METADONE.metadone.DOCUMENTS d
                   left join NOYAU.patient.PATIENT p on d.doc_pat_id = p.pat_id
                   left join STOCKAGE.stockage.FILES f on f.fil_id = d.doc_stockage_id
-            where doc_nom like '%%'
-                  and p.pat_ipp in ('0300758425')"""
+            where doc_nom like '%Anapath%'
+                  and p.pat_ipp in ({ipp_str})"""
 
 # Executing the query
-i=0
 with connection.cursor() as cursor:
     cursor.execute(query)
     while True:
-        i+=1
         row = cursor.fetchone()
         if row is None:
             break
-        if (row[5] is not None):
-            newfile = open('.././data/extract/'+str(i)+".pdf", 'wb')
-            newfile.write(row[5])
-            newfile.close()
+        if row[5] is not None:
+            pat_ipp = row[0]  # Get pat_ipp
+            doc_nom = row[1]  # Get doc_nom (original file name)
+            fil_data = row[5]  # Get file data
+            filename = f"../.././data/extract/{pat_ipp}_{doc_nom}.pdf"
+            with open(filename, 'wb') as newfile:
+                newfile.write(fil_data)
 # Closing cursor and connection
 cursor.close()
 connection.close()
