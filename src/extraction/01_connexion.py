2418d9276cbd856c1f983d3d7bf02af111ac3dfa
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2024-03-05 15:09:28 +0100

    running connexion with batches

--- src/extraction/01_connexion.py
+++ src/extraction/01_connexion.py
@@ -12,28 +12,24 @@ connection = pyodbc.connect(driver='{SQL Server}', host=server, port=1433, datab
 cursor = connection.cursor()
 
 
-filtered_identifiers = df[df['IPP'].astype(str).str.startswith('0')]['IPP'].tolist()
-
-batch_size = 499
+filtered_identifiers = df.loc[df['IPP'].astype(str).str.strip().str[0].str.isdigit(), 'IPP'].tolist()
+batch_size = 450
 batches = [filtered_identifiers[i:i+batch_size] for i in range(0, len(filtered_identifiers), batch_size)]
 formatted_batches = []
 for batch in batches:
-    formatted_batch = ", ".join(f"'{id}'" for id in batch)
-    formatted_batches.append(f"(p.pat_ipp in ({formatted_batch}))")
+    formatted_batch = ", ".join(f"{ipp}" for ipp in batch)
+    formatted_batches.append(f"({formatted_batch})")
 
+result_string = " OR pat_ipp IN ".join(formatted_batches)
 
-result_string = " OR ".join(formatted_batches)
-
-
-# Sample query
 query = f"""select p.pat_ipp, d.doc_nom, d.doc_creation_date, d.doc_realisation_date, d.doc_stockage_id, fil_data
         from METADONE.metadone.DOCUMENTS d
         left join NOYAU.patient.PATIENT p on d.doc_pat_id = p.pat_id
          left join STOCKAGE.stockage.FILES f on f.fil_id = d.doc_stockage_id
             WHERE doc_nom LIKE '%Anapath%'
-            and ({result_string})"""
+            and (p.pat_ipp IN {result_string})"""
 
-print(query)
+
 with connection.cursor() as cursor:
     cursor.execute(query)
     while True:
@@ -47,6 +43,7 @@ with connection.cursor() as cursor:
             filename = f"..//..//./data//extract//{pat_ipp}_{doc_stockage_id}.pdf"
             with open(filename, 'wb') as newfile:
                 newfile.write(fil_data)
-# Closing cursor and connection
+
+
 cursor.close()
 connection.close()
